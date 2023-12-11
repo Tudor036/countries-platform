6baaf4ba5efca31302a296337f8396de186ff119
tudorborca <borcatudor@gmail.com>
2023-12-11 19:36:25 +0200

    configure tailwind theme and add deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,31 @@
-import type { Config } from 'tailwindcss';
+import defaultTheme from "tailwindcss/defaultTheme";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	darkMode: ["class", '[data-color-theme="dark"]'],
+	content: ["./src/**/*.tsx"],
+	theme: {
+		extend: {
+			backgroundColor: {
+				primaryLight: "#ffffff",
+				secondaryLight: "#fafafa",
+				primaryDark: "#202C36",
+				secondaryDark: "#2B3844",
+			},
+			fontFamily: {
+				sans: [
+					"'Nunito Sans', sans-serif",
+					...defaultTheme.fontFamily.sans,
+				],
+			},
+			fontWeight: {
+				heading: "800",
+				body: "600",
+				detail: "300",
+			},
+		},
+	},
+	plugins: [],
 };
 
 export default config;
